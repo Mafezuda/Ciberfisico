f21116432343abe5eca3ca0ccdd88b5d5eb20fdf
Mafezuda <moreira.mafe@gmail.com>
2024-05-23 14:05:03 -0700

    associativo com lru

--- CacheAssociativo.java
+++ CacheAssociativo.java
@@ -1,57 +1,37 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CacheAssociativo implements Cache {
-    private int[] cache;
+public class CacheAssociativo {
+
+    private List<Integer> cache;
     private int tamanhoCache;
     private int acertos;
     private int falhas;
-    private List<Integer> lruList;
 
     public CacheAssociativo(int tamanhoCache) {
         this.tamanhoCache = tamanhoCache;
-        this.cache = new int[tamanhoCache];
+        this.cache = new ArrayList<>(tamanhoCache);
         this.acertos = 0;
         this.falhas = 0;
-        this.lruList = new ArrayList<>();
-
-        // Inicializa a cache com -1 para indicar que todas as linhas estão vazias
-        for (int i = 0; i < tamanhoCache; i++) {
-            cache[i] = -1;
-        }
     }
 
-    @Override
     public void acessarMemoria(int endereco) {
-        if (lruList.contains(endereco)) {
+        if (cache.contains(endereco)) {
             acertos++;
-            System.out.println("Acerto: Endereço " + endereco + " já está na cache");
-            lruList.remove((Integer) endereco);
-            lruList.add(endereco);
+            // Se o bloco já está na cache, move ele para o final da lista
+            cache.remove((Integer) endereco);
+            cache.add(endereco);
         } else {
             falhas++;
-            System.out.println("Falha: Endereço " + endereco + " não está na cache");
-
-            if (lruList.size() >= tamanhoCache) {
-                int enderecoARemover = lruList.remove(0);
-                for (int i = 0; i < tamanhoCache; i++) {
-                    if (cache[i] == enderecoARemover) {
-                        cache[i] = endereco;
-                        break;
-                    }
-                }
-            } else {
-                for (int i = 0; i < tamanhoCache; i++) {
-                    if (cache[i] == -1) {
-                        cache[i] = endereco;
-                        break;
-                    }
-                }
+            // Se a cache estiver cheia, remove o primeiro bloco (LRU) antes de adicionar o novo bloco
+            if (cache.size() >= tamanhoCache) {
+                cache.remove(0);
             }
-            lruList.add(endereco);
+            // Adicionamos o novo bloco na cache
+            cache.add(endereco);
         }
     }
 
-    @Override
     public void imprimirEstatisticas() {
         System.out.println("Total de acertos: " + acertos);
         System.out.println("Total de falhas: " + falhas);
